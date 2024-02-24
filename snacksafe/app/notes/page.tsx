3cca864a459ae17de31dc9a3b2c854ea13cb5d6c
Benjamin Chang <bchang@illinois.edu>
2024-02-23 22:02:48 -0600

    idek

--- snacksafe/app/notes/page.tsx
+++ snacksafe/app/notes/page.tsx
@@ -1,8 +1,8 @@
-import { createClient } from '@/utils/supabase/server';
-
+import { createClient } from "@/utils/supabase/server";
+import React, { ReactElement } from "react";
 export default async function Notes() {
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
 }
