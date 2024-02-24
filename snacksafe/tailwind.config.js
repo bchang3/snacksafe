bf35eed3c695ee948b48b5611e5ec2cd9a56e671
Benjamin Chang <bchang@illinois.edu>
2024-02-23 23:03:11 -0600

    Added fonts and colors

--- snacksafe/tailwind.config.js
+++ snacksafe/tailwind.config.js
@@ -5,14 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+			'grotesk-regular': ['grotesk-regular'],
+      'grotesk-light': ['grotesk-light'],
+      'grotesk-medium': ['grotesk-medium'],
+      'grotesk-bold': ['grotesk-bold'],
+
+		},
     extend: {
       colors: {
-        background: "hsl(var(--background))",
+        background: "#fefae0",
         foreground: "hsl(var(--foreground))",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
+        moss_green: {
+					DEFAULT: '#283618',
+				},
+        white: {
+					DEFAULT: '#FEFDFD',
+				},
+				beige: {
+					DEFAULT: '#fefae0',
+				}
       },
     },
   },
