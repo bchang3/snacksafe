7ac6c2d7c892781cfb04cf34755d5ef1685fdde4
Benjamin Chang <bchang@illinois.edu>
2024-02-25 04:20:32 -0600

    Used data from database

--- snacksafe/app/notes/page.tsx
+++ snacksafe/app/notes/page.tsx
@@ -3,11 +3,10 @@ import React, { ReactElement } from "react";
 export default async function Notes() {
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
-
+  const { data: restaurants } = await supabase.from("Restaurants").select();
   return (
     <div className="text-black">
-      hello
-      <pre>{JSON.stringify(notes, null, 2)}</pre>
+      <pre>{JSON.stringify(restaurants, null, 2)}</pre>
     </div>
   );
 }
